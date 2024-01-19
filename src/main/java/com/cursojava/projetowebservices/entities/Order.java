43805a7936bd81702c2f4942d4c06e314d25b02f
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2024-01-19 10:07:14 -0300

    User Insert

--- src/main/java/com/cursojava/projetowebservices/entities/Order.java
+++ src/main/java/com/cursojava/projetowebservices/entities/Order.java
@@ -88,6 +88,14 @@ public class Order implements Serializable {
         return items;
     }
 
+    public double getTotal() {
+        double sum = 0;
+        for (OrderItem orderItem : items) {
+            sum += orderItem.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/main/java/com/cursojava/projetowebservices/entities/Payment.java
+++ src/main/java/com/cursojava/projetowebservices/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.cursojava.projetowebservices.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -17,6 +18,7 @@ public class Payment implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private Instant moment;
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
